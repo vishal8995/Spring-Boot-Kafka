9fc40ac0d47f3544c0fdb775fedd639ac8231a27
Vishal <vishal8995@gmail.com>
2023-06-19 19:06:11 +0100

    Confluentic Images based docker-compose updated

---
+++ src/test/java/com/vishal/springbootkafka/SpringBootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
